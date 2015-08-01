8b02c279cbdceb3e0d3fc82e4921d1791018f0cc
ffandii <13593961606@163.com>
2015-08-01 21:55:34 +0800

    modified some place

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); //导航栏的提示框
+
+    //   $('div.hid-ul').show();
+//   $('ul.arc-list').removeClass('hidden');
+    $('div.cell').on('mouseover', function() {
+        $('div.hid-ul').show();
+    });
+
+    $('div.cell').on('mouseout', function() {
+        $('div.hid-ul').hide();
+    });
 });
