9690c630857f02be4c6231b2150ae65c5374de06
ffandii <13593961606@163.com>
2015-08-02 19:35:54 +0800

    modified some code for side bar

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); //导航栏的提示框
 
+    $('div.hid-ul').hide();
+
     //   $('div.hid-ul').show();
 //   $('ul.arc-list').removeClass('hidden');
+    $('div.cell').on('click', function() {
+        if($('div.hid-ul').hasClass('hiden')){
+            $('div.hid-ul').removeClass('hiden');
+            $('div.hid-ul').show('slow');
+        }
+        else {
+            $('div.hid-ul').addClass('hiden');
+            $('div.hid-ul').hide('slow');
+        }
+    });
+
+    //   $('div.hid-ul').show();
+//   $('ul.arc-list').removeClass('hidden');
+
+    /*
     $('div.cell').on('mouseover', function() {
         $('div.hid-ul').show();
     });
 
     $('div.cell').on('mouseout', function() {
         $('div.hid-ul').hide();
-    });
+    });*/
+
+
 
     //blog-outline部分的分割线
     $('div.blog-outline div.gap-line:nth-last-child(1)').remove();
