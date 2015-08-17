c5f292c3e8366a0a8feffc1f636fa8afb0344552
ffandii <13593961606@163.com>
2015-08-17 17:19:53 +0800

    modified js/main.js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,41 @@
+$('div.hid-ul').hide();
+
+//blog-outline部分的分割线
+$('div.blog-outline div.gap-line:nth-last-child(1)').remove();
+
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); //导航栏的提示框
 
-    $('div.hid-ul').hide();
-
-    //   $('div.hid-ul').show();
-//   $('ul.arc-list').removeClass('hidden');
+//------------------侧边栏的展开和折叠--------------------------
     $('div.cell').on('click', function() {
-        if($('div.hid-ul').hasClass('hiden')){
-            $('div.hid-ul').removeClass('hiden');
-            $('div.hid-ul').show(300);
+        if($(this).children().hasClass('hiden')){
+            $(this).children().removeClass('hiden');
+            $(this).find('div.hid-ul').show(300);
         }
         else {
-            $('div.hid-ul').addClass('hiden');
-            $('div.hid-ul').hide(300);
+            $(this).find('div.hid-ul').addClass('hiden');
+            $(this).find('div.hid-ul').hide(300);
         }
     });
 
-    //   $('div.hid-ul').show();
-//   $('ul.arc-list').removeClass('hidden');
+    $('div.cell').on('mouseenter',function(){
+        /*
+         var $details=$(this).find('span.sum');
+         $details.addClass('animation');*/
 
-    /*
-    $('div.cell').on('mouseover', function() {
-        $('div.hid-ul').show();
+
+        var $details=$(this).find('span');
+        if(!$details.hasClass('animation')){
+            $details.addClass('animation');
+        }
+        else {
+            var el= $details,
+                newone = el.clone(true);
+            el.before(newone);
+            $(this).find("." + el.attr("class") + ":last").remove();
+
+        }
+
     });
-
-    $('div.cell').on('mouseout', function() {
-        $('div.hid-ul').hide();
-    });*/
-
-
-
-    //blog-outline部分的分割线
-    $('div.blog-outline div.gap-line:nth-last-child(1)').remove();
+//-------------------------------------------------------------
 });
