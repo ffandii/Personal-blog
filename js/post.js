024bca46d5f49e3591a634bb71dbf1487627023b
ffandii <13593961606@163.com>
2015-08-20 19:46:39 +0800

    modified js/post.js

--- js/post.js
+++ js/post.js
@@ -9,4 +9,30 @@ $(document).ready(function() {
     $('div.p-section:nth-last-child(1)').css({
         'border':'none'
     });
+
+    /*****************给每一小节添加锚，便于阅读******************/
+    $('div.content div.p-section').each(function(index){
+        var idText="top"+(index+1);
+        $(this).attr("id",idText);
+    });
+
+    $('div.content div.p-section h3').wrap('<div class="section-header"></div>');
+
+    $('div.content div.p-section h3').each(function(index){
+        var hrefText="#top"+(index+1);
+        $('<a class="anchor" aria-hidden="true" href='+hrefText+' ><span class="icon glyphicon glyphicon-link"></span></a>')
+            .insertAfter(this).hide();
+    });
+
+    $('div.p-section div.section-header').on('mouseenter mouseleave',function(event){
+        if($(window).width()>=400){
+            if(event.type=='mouseenter'){
+                $(this).children('a').stop().fadeIn('slow');
+
+            }
+            else {
+                $(this).children('a').stop().fadeOut('slow');
+            }
+        }
+    });
 });
