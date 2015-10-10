2490c32c1cc0bae52b3deb5eba96b9826d64a5b6
ffandii <13593961606@163.com>
2015-10-10 09:11:22 +0800

    modified demo.js

--- js/demo.js
+++ js/demo.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); //导航栏的提示框
+
     $('div.demo-ui div.cell').hover(function() {
         $(this).find('.details').fadeTo('fast', 0.7);
     }, function() {
